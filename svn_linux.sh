3c8e3ca7790ea40601359c0ffe2a6585fbe16362
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-08-02 17:40:21 +0500

    Update svn_linux.sh

--- svn_linux.sh
+++ svn_linux.sh
@@ -6,3 +6,5 @@ svn st -q|grep M|cut -c9-|xargs   diffuse -n
 #write coommand to copy   all the modified files from base to some spefici folder
 
 svn st -q|grep M|cut -c9-|xargs i  cp "{}" ../project/copy_folder
+# top delete all temp wlft files
+svn st | grep wlft| cut -c9-|xargs -n1 rm 
